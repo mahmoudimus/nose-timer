a4f5f2515779ab377e514f02dce561ead82d53e3
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-09-18 19:48:48 -0300

    Added setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='nose-timer',
+    version='0.1',
+    description=u'A timer plugin for nosetests',
+    long_description=open('README.rst').read(),
+    author = u'Juan Pedro Fisanotti',
+    author_email = 'fisadev@gmail.com',
+    url='https://github.com/fisadev/nose-timer',
+    packages=['nosetimer', ],
+    license='LICENSE.txt',
+    entry_points = '''
+        [nose.plugins.0.10]
+        nosetimer = nosetimer:TimerPlugin
+    ''',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Environment :: Console',
+    ],
+)
