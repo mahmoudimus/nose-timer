1e047ff872c94874f9fe96cbb6690cc70835f81e
Ivan Kolodyazhny <e0ne@e0ne.info>
2013-09-21 00:49:02 +0300

    Add support for Python 3.1 and 3.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Testing',
         'Environment :: Console',
     ],
