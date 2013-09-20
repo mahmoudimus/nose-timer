e9d25888092b517bb765ee7adbec05d4bb22561a
Ivan Kolodyazhny <e0ne@e0ne.info>
2013-09-21 00:46:44 +0300

    Basic test infrastructure: 1. Stubs for some tests 2. tox.ini file with environments list

---
+++ tests/test_plugin.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TimerPlugin(unittest.TestCase):
+    def test_addOptions(self):
+        self.fail()
+
+    def test_format_report(self):
+        self.fail()
+
+    def test_timeTaken(self):
+        self.fail()
