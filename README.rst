3a00af1475b7139061956ae99e11102fe7d2e26f
Stanislav Kudriashev <skudriashev@griddynamics.com>
2014-02-11 11:59:59 +0200

    Added unit test for skipped test exception

--- README.rst
+++ README.rst
@@ -86,4 +86,4 @@ Contributors
 - `@e0ne <https://github.com/e0ne>`_
 - `@kevinburke <https://github.com/kevinburke>`_
 - `@DmitrySandalov <https://github.com/DmitrySandalov>`_
-
+- `@skudriashev <https://github.com/skudriashev>`_
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -5,29 +5,39 @@ import nosetimer
 
 
 class TestTimerPlugin(unittest.TestCase):
+
+    def setUp(self):
+        super(TestTimerPlugin, self).setUp()
+        self.plugin = nosetimer.TimerPlugin()
+        self.test_mock = mock.MagicMock(name='test')
+        self.test_mock.id.return_value = 1
+        self.opts_mock = mock.MagicMock(name='opts')
+
     def test_addOptions(self):
-        plugin = nosetimer.TimerPlugin()
         parser = mock.MagicMock()
-        plugin.addOptions(parser)
+        self.plugin.addOptions(parser)
         self.assertEquals(parser.add_option.call_count, 5)
 
     def test_configure(self):
-        attributes = ['config', 'timer_top_n', 'timer_ok',
-                      'timer_warning', '_timed_tests']
-        plugin = nosetimer.TimerPlugin()
+        attributes = ('config', 'timer_top_n', 'timer_ok',
+                      'timer_warning', '_timed_tests')
         for attr in attributes:
-            self.assertFalse(hasattr(plugin, attr))
+            self.assertFalse(hasattr(self.plugin, attr))
 
-        mock_opts = mock.MagicMock()
-        plugin.configure(mock_opts, None)
+        self.plugin.configure(self.opts_mock, None)
         for attr in attributes:
-            self.assertTrue(hasattr(plugin, attr))
+            self.assertTrue(hasattr(self.plugin, attr))
 
     def test_timeTaken(self):
-        plugin = nosetimer.TimerPlugin()
-        self.assertFalse(hasattr(plugin, '_timer'))
-        self.assertEquals(plugin._timeTaken(), 0.0)
+        self.assertFalse(hasattr(self.plugin, '_timer'))
+        self.assertEquals(self.plugin._timeTaken(), 0.0)
 
-        plugin.startTest(None)
-        self.assertTrue(hasattr(plugin, '_timer'))
-        self.assertNotEquals(plugin._timeTaken(), 0.0)
+        self.plugin.startTest(self.test_mock)
+        self.assertTrue(hasattr(self.plugin, '_timer'))
+        self.assertNotEquals(self.plugin._timeTaken(), 0.0)
+
+    def test_afterTest(self):
+        self.opts_mock.timer_verbose.return_value = True
+        self.plugin.configure(self.opts_mock, None)
+        # make sure there is no exception if test time result was not found
+        self.plugin.afterTest(self.test_mock)
